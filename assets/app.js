e60d254ca142e3f24f92f81a2f2790cb3914c8eb
Thierry BROUET <56256915+thierryDev62@users.noreply.github.com>
2021-03-06 18:00:30 +0100

    [update]

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello React!</h1>
+        </div>
+)
+    ;
+};
+
+export default App;
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
